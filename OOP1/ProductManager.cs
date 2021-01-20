fea50cef3a9401458250a5877aca4a01806e093f
FURKAN UYSAL <uysalnet@hotmail.com>
2021-01-20 23:50:40 +0300

    2021.01.20 Ders sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+        }
+
+        public int Sum(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+
+        }
+
+        public void Sum2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+    }
+}
