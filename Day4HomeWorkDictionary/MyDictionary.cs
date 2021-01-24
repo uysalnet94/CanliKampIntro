800b88c5b76ffd59ebb1a9d8c87253ec29cf119e
FURKAN UYSAL <uysalnet@hotmail.com>
2021-01-24 23:18:53 +0300

    Day 4 Homework is done

---
+++ Day4HomeWorkDictionary/MyDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4HomeWorkDictionary
+{
+    class MyDictionary<T, D>
+    {
+        // Dictionary lerde iki farklı eleman tipi verilebileceği  için iki tane array tanımı yaptım.
+
+        //kullanılacak olan ana arrayler
+        T[] items;
+        D[] items2;
+
+        public MyDictionary()
+        {
+            items = new T[0];
+            items2 = new D[0];
+        }
+
+        public void Add(T item, D item2)
+        {
+            // Yedek arrayler ve yedekleme
+            T[] tempArray = items;
+            D[] tempArray2 = items2;
+
+            // ana arraylere eski boyutundan bir fazla alan ekleme ve yenileme
+            items = new T[items.Length + 1];
+            items2 = new D[items2.Length + 1];
+
+            // eski değerleri atama işlemleri
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                items2[i] = tempArray2[i];
+            }
+
+            //Yeni İtem ın eklenmesi
+            items[items.Length - 1] = item;
+            items2[items2.Length - 1] = item2;
+        }
+
+        public T[] Items1
+        {
+            get
+            {
+                return items;
+            }
+        }
+        public D[] Items2
+        {
+            get
+            {
+                return items2;
+            }
+        }
+    }
+}
---
+++ Day4HomeWorkDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day4HomeWorkDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+
+            dictionary.Add(437, "Furkan");
+            dictionary.Add(596, "Fatma");
+            dictionary.Add(852, "Ali");
+            dictionary.Add(796, "Gamze");
+            dictionary.Add(793, "Büşra");
+            dictionary.Add(798, "Seda");
+
+            for (int i = 0; i < dictionary.Items1.Length; i++)
+            {
+                Console.WriteLine("Öğrenci No : " + dictionary.Items1[i] + "  Öğrenci Adı : " + dictionary.Items2[i]);
+            }
+
+
+        }
+    }
+}
